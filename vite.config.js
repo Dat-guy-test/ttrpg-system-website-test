c6584ac72a5f60a3eb04078d9d1bd39e0c078d62
Dat-guy-test <janek.pytkowski@gmail.com>
2025-02-22 21:53:32 +0100

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
  * @type {import('vite').UserConfig}
  */
 export default{
-    base: process.env.NODE_ENV === 'production' ? '/ttrpg-system-website-test/' : '/ttrpg-system-website-test/'
+    base: process.env.NODE_ENV === 'production' ? '/ttrpg-system-website-test/' : ''
 }
